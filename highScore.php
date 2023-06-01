1d4572b30b511e383c0a581853141e19c746a19b
jonascantcode <jomaa058@osloskolen.no>
2023-06-01 09:32:03 +0200

    fixed sql syntax

--- highScore.php
+++ highScore.php
@@ -3,14 +3,14 @@ include 'dbconn.php';
 
 $sql = "SELECT * FROM high_score ORDER BY score DESC LIMIT 10";
 
-$resultat = $kobling->query($sql);
+$result = $connect->query($sql);
 
-if (!$resultat) {
-    echo "MAYDAY MAYDAY: $sql ($kobling->error).";
+if (!$result) {
+    echo"Failed to connect" . $sql ($connect->error);
 } else {
     echo '<table><tr><th>Navn</th><th>Score</th></tr>';
     
-    while ($rad = $resultat->fetch_assoc()) {
+    while ($rad = $result->fetch_assoc()) {
         $navn = $rad["name"];
         $score = $rad["score"];
         echo '<tr><td>' . $navn . '</td><td>' . $score . '</td></tr>';
