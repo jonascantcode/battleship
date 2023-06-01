d6761305562d4fd049efe862f9a64ffa11212340
jonascantcode <jomaa058@osloskolen.no>
2023-06-01 10:13:24 +0200

    faq sql and php now available

---
+++ faq.php
@@ -0,0 +1,46 @@
+<h1>Frequently Asked Questions</h1>
+
+<!-- Question submission form -->
+<h2>Submit a Question</h2>
+<form method="post">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required><br><br>
+    <label for="question">Question:</label>
+    <textarea id="question" name="question" rows="4" cols="50" required></textarea><br><br>
+    <input type="submit" value="Submit">
+</form>
+
+<h1>Frequently Asked Questions</h1>
+
+<?php
+include 'dbconn.php';
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'] ?? '';
+    $question = $_POST['question'] ?? '';
+
+    // Insert question into the database
+    $sql = "INSERT INTO faq (name, question) VALUES ('$name', '$question')";
+    if ($connect->query($sql) === TRUE) {
+        echo "Question submitted successfully.";
+    } else {
+        echo "Error submitting question: " . $connect->error;
+    }
+}
+
+// Fetch and display questions from the database
+$sql = "SELECT * FROM faq";
+$result = $connect->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<div>";
+        echo "<strong>Name: </strong>" . $row['name'] . "<br>";
+        echo "<strong>Question: </strong>" . $row['question'];
+        echo "</div><br>";
+    }
+} else {
+    echo "No questions found.";
+}
+
+?>
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- -----------------------------------------------------
 -- Scheme battlship
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `battlehship` DEFAULT CHARACTER SET utf8 COLLATE utf8_danish_ci ;
-USE `battleship` ;
+CREATE DATABASE IF NOT EXISTS `battleship` DEFAULT CHARACTER SET utf8 COLLATE utf8_danish_ci ;
+
 
 -- -----------------------------------------------------
 -- Table `battleship`.`high_score`
@@ -11,5 +11,12 @@ CREATE TABLE IF NOT EXISTS `battleship`.`high_score` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(3) NOT NULL,
   `score` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `battleship`.`faq` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(56) NOT NULL,
+  `question` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
